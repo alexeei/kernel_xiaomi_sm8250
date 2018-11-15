3a95d5093479af4b8761db1ffe121659106a4ac4
Mark Rutland <mark.rutland@arm.com>
2018-11-15 22:41:58 +0000

    linkage: add generic GLOBAL() macro
    Declaring a global symbol in assembly is tedious, error-prone, and
    painful to read. While ENTRY() exists, this is supposed to be used for
    function entry points, and this affects alignment in a potentially
    undesireable manner.
    
    Instead, let's add a generic GLOBAL() macro for this, as x86 added
    locally in commit:
    
      95695547a7db44b8 ("x86: asm linkage - introduce GLOBAL macro")
    
    ... thus allowing us to use this more freely in the kernel.
    
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: AKASHI Takahiro <takahiro.akashi@linaro.org>
    Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Torsten Duwe <duwe@suse.de>
    Cc: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>

--- include/linux/linkage.h
+++ include/linux/linkage.h
@@ -103,10 +103,9 @@
 #define ALIGN __ALIGN
 #define ALIGN_STR __ALIGN_STR
 
-/* === DEPRECATED annotations === */
 
 #ifndef GLOBAL
-/* deprecated, use SYM_DATA*, SYM_ENTRY, or similar */
+
 #define GLOBAL(name) \
 	.globl name ASM_NL \
 	name:
