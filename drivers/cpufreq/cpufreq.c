bdba93a7f40f7a548152021cf272acfafa780db3
Viresh Kumar <viresh.kumar@linaro.org>
2022-05-26 17:21:21 +0530

    cpufreq: Drop unnecessary cpus locking from store()
    This change was introduced long back by commit 4f750c930822 ("cpufreq:
    Synchronize the cpufreq store_*() routines with CPU hotplug").
    
    Since then, both cpufreq and hotplug core have been reworked and have
    much better locking in place. The race mentioned in commit 4f750c930822
    isn't possible anymore.
    
    Drop the unnecessary locking.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -942,20 +942,11 @@ static ssize_t store(struct kobject *kobj, struct attribute *attr,
 	if (!fattr->store)
 		return -EIO;
 
-	/*
-	 * cpus_read_trylock() is used here to work around a circular lock
-	 * dependency problem with respect to the cpufreq_register_driver().
-	 */
-	if (!cpus_read_trylock())
-		return -EBUSY;
 
-	if (cpu_online(policy->cpu)) {
-		down_write(&policy->rwsem);
+	down_write(&policy->rwsem);
+	if (likely(!policy_is_inactive(policy)))
 		ret = fattr->store(policy, buf, count);
-		up_write(&policy->rwsem);
-	}
-
-	cpus_read_unlock();
+	up_write(&policy->rwsem);
 
 	return ret;
 }
