980f611dba71ad35edb2695acc5752486a57eb3b
alexeei <megaxaser@gmail.com>
2025-06-21 13:51:18 +0300

    fix build

--- kernel/sys.c
+++ kernel/sys.c
@@ -1250,7 +1250,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4)) {
 		strcpy(tmp.release, "5.4.186");
-		pr_debug("fake uname: %s/%d release=%s\n",Add commentMore actions
+		pr_debug("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
 	}
 	up_read(&uts_sem);
