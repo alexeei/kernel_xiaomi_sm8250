019cc9304de95676f8c610ec2031b55477c14e03
alexeei <megaxaser@gmail.com>
2026-05-03 17:49:43 +0300

    sched/eevdf: O(1) fastpath for task selection

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -906,6 +906,7 @@ struct sched_entity *__pick_first_entity(struct cfs_rq *cfs_rq)
 static struct sched_entity *pick_eevdf(struct cfs_rq *cfs_rq)
 {
 	struct rb_node *node = cfs_rq->tasks_timeline.rb_root.rb_node;
+    struct sched_entity *se = __pick_first_entity(cfs_rq);
 	struct sched_entity *curr = cfs_rq->curr;
 	struct sched_entity *best = NULL;
     
@@ -914,7 +915,7 @@ static struct sched_entity *pick_eevdf(struct cfs_rq *cfs_rq)
 	 * in this cfs_rq, saving some cycles.
 	 */
 	if (cfs_rq->nr_running == 1)
-		return curr && curr->on_rq ? curr : __node_2_se(node);
+		return curr && curr->on_rq ? curr : se;
 
 	if (curr && (!curr->on_rq || !entity_eligible(cfs_rq, curr)))
 		curr = NULL;
@@ -927,8 +928,14 @@ static struct sched_entity *pick_eevdf(struct cfs_rq *cfs_rq)
 	if (sched_feat(RUN_TO_PARITY) && curr && curr->vlag == curr->deadline)
 		return curr;
 
+    /* Pick the leftmost entity if it's eligible */
+	if (se && entity_eligible(cfs_rq, se)) {
+		best = se;
+		goto found;
+	}
+
 	while (node) {
-		struct sched_entity *se = __node_2_se(node);
+		
         struct rb_node *left = node->rb_left;
 
 		/*
@@ -940,6 +947,8 @@ static struct sched_entity *pick_eevdf(struct cfs_rq *cfs_rq)
 			continue;
 		}
 
+        se = __node_2_se(node);
+
 		/*
 		 * If this entity has an earlier deadline than the previous
 		 * best, take this one. If it also has the earliest deadline
@@ -947,21 +956,14 @@ static struct sched_entity *pick_eevdf(struct cfs_rq *cfs_rq)
 		 */
 		if (entity_eligible(cfs_rq, se)) {
 			best = se;
-			
-
-		
 			break;
-
-		
 		}
 
 		node = node->rb_right;
 	}
 
         
-
-
-
+found:
 if (!best || (curr && entity_before(curr, best)))
 		best = curr;
 
