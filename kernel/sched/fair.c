047fa5f60563e7fd9289945577ca254abb853a19
alexeei <megaxaser@gmail.com>
2026-05-10 22:02:24 +0300

    sched: Prevent raising SCHED_SOFTIRQ when CPU is !active

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -12949,8 +12949,12 @@ void trigger_load_balance(struct rq *rq)
 	if (unlikely(on_null_domain(rq)) || cpu_isolated(cpu_of(rq)))
 		return;
 #else
-	/* Don't need to rebalance while attached to NULL domain */
-	if (unlikely(on_null_domain(rq)))
+	/*
+	 * Don't need to rebalance while attached to NULL domain or
+	 * runqueue CPU is not active
+	 */
+	if (unlikely(on_null_domain(rq) || !cpu_active(cpu_of(rq))))
+		return;
 #endif
 
 	if (time_after_eq(jiffies, rq->next_balance))
