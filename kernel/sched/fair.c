0472b2e15bfa0d6865c3e440a7886c24f07a5fc9
alexeei <megaxaser@gmail.com>
2026-05-10 23:22:15 +0300

    sched/uclamp: Make task_fits_capacity() use util_fits_cpu()

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4645,9 +4645,12 @@ static inline bool task_fits_capacity(struct task_struct *p,
 	return capacity * 1024 > uclamp_task_util(p) * margin;
 }
 #else
-static inline int task_fits_capacity(struct task_struct *p, long capacity)
+static inline int task_fits_cpu(struct task_struct *p, int cpu)
 {
-	return fits_capacity(uclamp_task_util(p), capacity);
+	unsigned long uclamp_min = uclamp_eff_value(p, UCLAMP_MIN);
+	unsigned long uclamp_max = uclamp_eff_value(p, UCLAMP_MAX);
+	unsigned long util = task_util_est(p);
+	return util_fits_cpu(util, uclamp_min, uclamp_max, cpu);
 }
 #endif
 
@@ -4764,7 +4767,7 @@ static inline void update_misfit_status(struct task_struct *p, struct rq *rq)
 #ifdef CONFIG_SCHED_WALT
 	if (task_fits_max(p, cpu_of(rq))) {
 #else
-        if (task_fits_capacity(p, cpu_of(rq))) {
+       if (task_fits_cpu(p, cpu_of(rq))) {
 #endif
 		rq->misfit_task_load = 0;
 		return;
@@ -7959,7 +7962,7 @@ static void find_best_target(struct sched_domain *sd, cpumask_t *cpus,
 			 */
 			new_util = max(min_util, new_util);
 			if ((!(prefer_idle && idle_cpu(i)) &&
-			    new_util > capacity_orig) ||
+			    new_util > capacity_orig) || !task_fits_cpu(p, capacity_orig))
 			     (is_min_capacity_cpu(i) &&
 #ifdef CONFIG_SCHED_WALT
 			      !task_fits_capacity(p, capacity_orig, i)))
